f94125f8336f94dedd792071a1f3fc1084faecbb
Kayro França <k4yro01@gmail.com>
2021-01-18 14:47:24 -0300

    added component 'Why Choose Easybank'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import WhyChoose from './components/WhyChoose';
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <WhyChoose />
     </>
   );
 }
---
+++ src/components/WhyChoose/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+const WhyChoose = () => {
+    return (
+        <div className="wce-container">
+            <h1>Why choose Easybank?</h1>
+            <p>We leverage Open Banking to turn your bank account into your financial hub. <br/>
+                Control your finances like never before.</p>
+            <div className="wce-list">
+                
+            </div>
+        </div>
+    );
+}
+
+export default WhyChoose;
