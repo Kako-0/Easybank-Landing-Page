d240a91e9fe83ca14bcaa08709c9308664a41088
Kayro França <k4yro01@gmail.com>
2021-01-19 13:52:50 -0300

    update style footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Articles from './components/Articles';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
 import WhyChoose from './components/WhyChoose';
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <WhyChoose />
       <Articles />
+      <Footer />
     </>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './style.css';
+import '../../styles/btn.css'
+
+import logo from '../../images/logo-white.svg';
+import { ReactComponent as YoutubeIcon } from "../../images/icon-youtube.svg";
+import { ReactComponent as FacebookIcon } from "../../images/icon-facebook.svg";
+import { ReactComponent as TwitterICon } from "../../images/icon-twitter.svg";
+import { ReactComponent as PinterestIcon } from "../../images/icon-pinterest.svg";
+import { ReactComponent as InstagramIcon } from "../../images/icon-instagram.svg";
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="footer-container">
+            <div className="footer-icons">
+                <img src={logo} alt="logo" />
+                <ul className="footer-icon-social">
+                    <li><FacebookIcon /></li>
+                    <li><YoutubeIcon /></li>
+                    <li><TwitterICon /></li>
+                    <li><PinterestIcon /></li>
+                    <li><InstagramIcon /></li>
+                </ul>
+            </div>
+            <ul className="footer-list">
+                <li>About Us</li>
+                <li>Carrers</li>
+                <li>Contact</li>
+                <li>Support</li>
+                <li>Blog</li>
+                <li>Privacy Policy</li>
+            </ul>
+            <div className="btn-copyright">
+                <button className="btn-main btn-footer">Request Invite</button>
+                <p>© Easybank. All Rights Reserved</p>
+            </div>
+        </footer>
     );
 }
 
