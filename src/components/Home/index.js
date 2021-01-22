f639946302640836f2ccc73fc0ffea613cbab167
Kayro França <k4yro01@gmail.com>
2021-01-22 19:14:51 -0300

    update mobile style Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import '../../styles/btn.css'
 
 
 import Background from "../../images/bg-intro-desktop.svg";
+import BackgroundMobile from "../../images/bg-intro-mobile.svg";
 import Mockups from '../../images/image-mockups.png';
 
 const Home = () => {
@@ -15,14 +16,18 @@ const Home = () => {
                     digital banking
                 </h1>
                 <p>
-                    Take your financial life online. Your Easybank account <br/> 
-                    will be a one-stop-shop for spending, saving, <br/> 
+                    Take your financial life online. Your Easybank account
+                    will be a one-stop-shop for spending, saving, 
                     budgeting, investing, and much more.
                 </p>
                 <button className="btn-main">Request Invite</button>
             </div>
             <div className="photos-home">
-                <img className="background-home" src={Background} alt="background"/>
+                <picture className="background-home">
+                    <source media="(max-width:420px)" srcSet={BackgroundMobile} />
+                    <source media="(min-width:1100px)" srcSet={Background} />
+                    <img src={Background} alt="background" />
+                </picture>
                 <div className="mockup-home"> <img  src={Mockups} alt="mockups" /></div>
                 
             </div>
