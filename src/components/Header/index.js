a173a5c6ff2e962658b9b6e2662c48a9a0b04eee
Kayro França <k4yro01@gmail.com>
2021-01-25 19:11:24 -0300

    refactoring component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useEffect,useState} from 'react';
 import './style.css';
 import '../../styles/btn.css';
+import hamburguer from '../../images/icon-hamburger.svg';
+import close from '../../images/icon-close.svg';
 import { ReactComponent as Logo } from '../../images/logo.svg';
 
+const List = () => {
+    return(
+    <ul className="content-header">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Blog</li>
+        <li>Careers</li>
+    </ul>
+)}
+
+const DesktopNav = () => {
+    return(
+    <>
+        <List />
+        <button className="btn-main btn-header"> Request Invite</button>
+    </>
+)}
+
+const MobileList = () => {
+    return(
+        <div className="mobile-list">
+            <List />
+        </div>
+    )
+}
+
+const MobileNav= () => {
+    const [isActive, setisActive] = useState(false);
+    return(
+    <>
+        <img className="options-header" src={hamburguer} alt="navegation" 
+            onClick={()=>{
+                setisActive(!isActive);
+            }}
+        />
+        {isActive && <MobileList />};
+    </>
+)}
+
 const Header = () => {
+    const [isMobile, setisMobile] = useState(window.matchMedia('(max-width:420px)').matches);
+    useEffect(()=>{
+        window.addEventListener("resize", ()=>{
+            setisMobile(window.matchMedia('(max-width:420px)').matches)
+        })
+    })
     return (
-        <header className="container-header">
+        <nav className="container-header">
             <a href="./" className="logo"> <Logo /></a>
-            <ul className="content-header">
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Blog</li>
-                <li>Careers</li>
-            </ul>
-            <button className="btn-main btn-header"> Request Invite</button>
-        </header>
+            
+            {isMobile ? <MobileNav /> : <DesktopNav />}
+        </nav>
     );
 }
 
